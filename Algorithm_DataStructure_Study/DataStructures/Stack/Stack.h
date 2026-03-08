6519de43a6f1cdb022a7c5a990d3900df7cb76c6
WonHyung Lee <80526272+link8879@users.noreply.github.com>
2026-03-08 23:28:37 +0900

    update stack.h

--- Algorithm_DataStructure_Study/DataStructures/Stack/Stack.h
+++ Algorithm_DataStructure_Study/DataStructures/Stack/Stack.h
@@ -6,13 +6,14 @@ public:
 	void Push(int data);	//スタックにデータを追加
 	void Pop();	//スタックからデータを削除
 	void Display();	//スタックの内容を表示
-	void top();	//スタックのトップのデータを表示
+	void Top();	//スタックのトップのデータを表示
 	bool isEmpty();	//スタックが空かどうかを表示
 	int size();	//スタックのサイズを表示
 	void Clear();	//スタックを空にする
-	void ~Stack();	//スタックのデストラクタ
-	void Stack(int capacity);
+	~Stack();	//スタックのデストラクタ
+	Stack(int capacity);
 private:
 	int* stackArray;	//スタックの配列
 	int topIndex = -1;	//スタックのトップのインデックス
+	int capacity;	//スタックの容量
 };
